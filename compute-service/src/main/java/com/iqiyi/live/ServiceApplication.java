5373228bd38856ba1f0523db3c7ed05fd1724d77
albert <zhaowenbo@qiyi.com>
2017-01-18 16:24:22 +0800

    add eureka server and service

---
+++ compute-service/src/main/java/com/iqiyi/live/ServiceApplication.java
@@ -0,0 +1,16 @@
+package com.iqiyi.live;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Desc : compute service
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main( String[] args ) {
+        new SpringApplicationBuilder(ServiceApplication.class).web(true).run(args);
+    }
+}
